e4d0b6e09dac94ad4d41a6d446e3ac5524a61192
Frederik Hahne <atomfrede@gmail.com>
2024-04-07 20:57:52 +0200

    initial commit

---
+++ src/main/java/io/github/atomfrede/htmx_and_javaland/HtmxAndJavalandApplication.java
@@ -0,0 +1,13 @@
+package io.github.atomfrede.htmx_and_javaland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxAndJavalandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmxAndJavalandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/atomfrede/htmx_and_javaland/HtmxAndJavalandApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.atomfrede.htmx_and_javaland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmxAndJavalandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
