4424de6f7585b3c87fe250b367f58258149b87b8
Frederik Hahne <atomfrede@gmail.com>
2024-04-09 15:49:05 +0200

    more samples

--- src/main/java/io/github/atomfrede/htmx_and_javaland/server_send_events/ServerSendEventsController.java
+++ src/main/java/io/github/atomfrede/htmx_and_javaland/server_send_events/ServerSendEventsController.java
@@ -2,20 +2,26 @@ package io.github.atomfrede.htmx_and_javaland.server_send_events;
 
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxTrigger;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import social.bigbone.MastodonClient;
 import social.bigbone.api.Pageable;
+import social.bigbone.api.Range;
 import social.bigbone.api.entity.Status;
 import social.bigbone.api.exception.BigBoneRequestException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static social.bigbone.api.method.TimelineMethods.StatusOrigin.LOCAL_AND_REMOTE;
 
 @Controller
 public class ServerSendEventsController {
+
+    final List<SseEmitter> emitters = new ArrayList<>();
 
     final MastodonClient client = new MastodonClient.Builder("mastodon.social").accessToken(System.getenv("ACCESS_TOKEN")).build();
 
@@ -24,13 +30,33 @@ public class ServerSendEventsController {
     public String index() {
         return "server-send-events/index";
     }
+
     @GetMapping("/server-send-events/sse-emitter")
     public SseEmitter progressEvents() throws BigBoneRequestException {
         SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
 
-        emit(sseEmitter);
+        emitters.add(sseEmitter);
 
         return sseEmitter;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    void shedule() throws BigBoneRequestException {
+        final Pageable<Status> statuses = client.timelines()
+                .getTagTimeline("javaland", LOCAL_AND_REMOTE).execute();
+        statuses.getPart().forEach(it -> {
+            emitAll(it.getContent());
+        });
+    }
+
+    void emitAll(String content) {
+
+        emitters.forEach(it -> {
+            try {
+                it.send(content);
+            } catch (IOException e) {
+            }
+        });
     }
 
     @Async
